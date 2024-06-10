5dc07f6afc50465efd52a590487eeb377dec37fd
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-06-10 17:18:07 +0530

    almost completed the task

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ufs_mashine_test/view/pages/home_page.dart';
 
 void main() {
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
